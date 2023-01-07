6bc8117920c2273d6a9d367d3afd342be90d8b00
sibautista2010 <sibautista2010@gmail.com>
2023-01-07 12:47:41 +0100

    Prueba1

---
+++ PrimeraClase.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class Gato {
+    private:
+        string nombre;
+        int edad;
+        int hDormido;
+    public:
+        Gato (string, int, int);
+        void come (void);
+        void duerme (void);
+        void muestraGato (void);
+};
+
+Gato::Gato (string _nombre, int _edad, int _hDormido) {
+    nombre = _nombre;
+    edad = _edad;
+    hDormido = _hDormido;
+}
+void Gato::come () {
+    cout << "El gato " << nombre << " está comiendo" << endl;
+}
+
+void Gato::duerme () {
+    cout << "El gato" << nombre << " está durmiendo" << endl;
+}
+
+void Gato::muestraGato () {
+    cout << "Los datos del gato " << nombre << " son :" << endl;
+    cout << "Nombre: " << nombre << endl;
+    cout << "Edad: " << edad << endl;
+    cout << "Horas de sueño: " << hDormido << endl;
+
+}
+
+
+int main () {
+    
+    Gato Meof ("Meof", 8, 12);
+    cout << "Primer programa con uso de clases en C++" << endl;
+    Meof.come();
+    Meof.duerme();
+    Meof.muestraGato();
+
+    return 0;
+}
